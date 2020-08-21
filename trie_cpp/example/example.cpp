8ad102fb8cd1923b8d43717018c50faa5288756c
Dan <ddumitre7@gmail.com>
2020-08-21 13:32:55 +0100

    Adding move and copy operations for the trie

--- trie_cpp/example/example.cpp
+++ trie_cpp/example/example.cpp
@@ -5,30 +5,72 @@
 #include "trie.h"
 
 using namespace std;
-using namespace triex;
+using namespace triecont;
+
+void printer(const vector<string>&);
 
 int main() {
   cout << "But hello!\n";
 
-  Trie t;
+  Trie t1;
 
-  t.addString("copy")
+  t1.addString("copy")
       .addString("cosmic")
       .addString("cosmos")
       .addString("cosmopolitan")
       .addString("copper")
       .addString("condition");
 
-  t.addString("mood")
+  t1.addString("mood")
       .addString("moody")
       .addString("moon")
       .addString("moonbeam")
       .addString("moonlight")
       .addString("moose")
-      .addString("mood") // Add this word again
-      .addString("moody"); // Add this word again
+      .addString("mood")    // Add this word again
+      .addString("moody");  // Add this word again
 
-  t.print();
+  auto ret_vector = t1.allWords();
+
+  cout << "________________________________" << endl;
+
+  printer(t1.allWords());
+
+  auto t2{t1};
+  cout << "________________________________" << endl;
+
+  printer(t2.allWords());
+
+  cout << "==================================" << endl;
+
+    t1.addString("convenient");
+
+  printer(t1.allWords());
+
+cout << "==================================" << endl;
+
+    t2.addString("monster");
+
+  printer(t2.allWords());
+
+
+  printer(t2.allWords());
+
+
+
+auto t3{t2};
+
+std::cout << std::boolalpha;
+
+cout << (t2 == t3) << endl;
+
+
 
   return 0;
+}
+
+void printer(const vector<string>& vec) {
+  for (const auto& e : vec) {
+    cout << e << endl;
+  }
 }
--- trie_cpp/src/trie.h
+++ trie_cpp/src/trie.h
@@ -1,24 +1,41 @@
-#ifndef TRIEX_TRIE_H_
-#define TRIEX_TRIE_H_
+#ifndef TRIECONT_TRIE_H_
+#define TRIECONT_TRIE_H_
+
+#include <assert.h>
 
 #include <iostream>
+#include <list>
 #include <map>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-namespace triex {
+namespace triecont {
 
 struct TrieNode;
 
 class Trie {
  public:
   Trie() : root_{new TrieNode} {}
-  Trie(const Trie &t) { root_ = t.root_; }
-  Trie(Trie &&t) {
-    root_ = t.root_;
-    t.root_ = nullptr;
+  Trie(const Trie &t) { root_ = t.root_->clone(); }
+  Trie(Trie &&t) : root_{t.root_} { t.root_ = nullptr; }
+
+  Trie &operator=(const Trie &t) {
+    if (this == &t) return *this;
+
+    Trie tmp{t};
+    swap(tmp, *this);
+    return *this;
   }
+
+  Trie &operator=(Trie &&t) {
+    swap(root_, t.root_);
+    return *this;
+  }
+
+  bool operator==(const Trie &t) { return root_->isEqual(t.root_); }
+  bool operator!=(const Trie &t) { return *this == t; }
 
   ~Trie() {
     cout << "Delete trie:" << endl;
@@ -41,7 +58,26 @@ class Trie {
     return *this;
   }
 
-  void print() { printRecursively(root_); }
+  vector<string> allWords() {
+    vector<string> v;
+    string s;
+
+    assert(root_);
+
+    for (auto e : root_->map_) {
+      s.push_back(e.first);
+      allWordsRecursively(e.second, s, v);
+      s.pop_back();
+    }
+
+    return v;
+  }
+
+  vector<string> wordsByChar(char ch) {
+    vector<string> v;
+
+    return v;
+  }
 
  private:
   struct TrieNode {
@@ -54,28 +90,61 @@ class Trie {
         delete e.second;
       }
     }
+
+    TrieNode *clone() {
+      auto t = new TrieNode;
+      t->is_leaf_ = is_leaf_;
+
+      for (auto e : map_) {
+        t->map_[e.first] = e.second->clone();
+      }
+      return t;
+    }
+
+    bool isEqual(const TrieNode *t) {
+      if (!t) return false;
+
+      if (is_leaf_ != t->is_leaf_) return false;
+
+      auto it1 = map_.begin();
+      auto it2 = t->map_.begin();
+
+      while (it1 != map_.end() && (it2 != t->map_.end())) {
+        if (it1->second->isEqual(it2->second)) {
+          ++it1;
+          ++it2;
+        } else
+          return false;
+      }
+
+      if ((it1 == map_.end()) && (it2 == t->map_.end()))
+        return true;
+      else
+        return false;
+    }
   };
 
   TrieNode *root_;
 
-  static void printRecursively(TrieNode *root) {
+  void allWordsRecursively(TrieNode *root, string &s, vector<string> &v) {
     if (!root) return;
 
-    if (root->is_leaf_) {
-      cout << "\n";
+    if (root->is_leaf_) v.push_back(s);
+
+    for (auto e : root->map_) {
+      s.push_back(e.first);
+      allWordsRecursively(e.second, s, v);
+      s.pop_back();
     }
 
-    for (const auto &e : root->map_) {
-      cout << e.first << " ";
-      printRecursively(e.second);
-    }
+    return;
   }
 };
 
-//DD:TODO
+// DD:TODO
 // Return allocator
 // Traits
 
-}  // namespace triex
+}  // namespace triecont
 
-#endif  // TRIEX_TRIE_H_
+#endif  // TRIECONT_TRIE_H_
